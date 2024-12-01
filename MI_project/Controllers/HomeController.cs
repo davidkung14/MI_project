5f2f30166f919f904d477bc2cc8f88df970168f0
USER <USER@DESKTOP-88IPED6>
2024-12-01 14:47:03 +0800

    加入專案檔案。

---
+++ MI_project/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using MI_project.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MI_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //return View();
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "dashboard/index.html"), "text/html");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MI_project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MI_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
